38c7d294afa396ac23d5e317927842cddece7905
Wakotu <442444286@qq.com>
2025-01-14 15:16:57 +0800

    update run.sh to run gunicorn in the background

--- run.sh
+++ run.sh
@@ -3,7 +3,10 @@
 log_dir='log'
 
 if [[ ! -d "$log_dir" ]]; then
-	mkdir "$log_dir"
+  mkdir "$log_dir"
 fi
 
-gunicorn --access-logfile "$log_dir"/access_log --error-logfile "$log_dir"/error_log interface_main:app -c gunicorn.conf.py     ##--log-level=debug
+gunicorn --access-logfile "$log_dir"/access_log --error-logfile "$log_dir"/error_log interface_main:app -c gunicorn.conf.py & ##--log-level=debug s
+
+pid="$!"
+echo "$pid" >"$log_dir"/pid
